5af628d4c105968c4b6480f3a24a656aa4923c5d
mehdi-hssn <hssnmehdi75@outlook.fr>
2022-11-30 16:47:57 +0100

    primer comit

---
+++ livre-vue/livre-vue/src/components/cards.vue
@@ -0,0 +1,73 @@
+<template>
+<main>
+
+<section class="py-5 text-center container">
+  <div class="row py-lg-5">
+    <div class="col-lg-6 col-md-8 mx-auto">
+      <h1 class="font-weight-light">Nos Livres</h1>
+      <p class="lead text-muted">Voici quelque uns de nos livres.<br/>
+        Ayez la possibilité de discuter et de commenter les oeuvres avec d'autre lecteurs.
+      </p>
+    </div>
+  </div>
+</section>
+
+<div class="album py-5 bg-light">
+  <div class="container">
+
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <div v-for="produit in produits" :key="produit.id" class="col">
+        <div class="card shadow-sm">
+          <img :src="produit.image" class="bd-placeholder-img card-img-top" width="250" height="500 " xmlns="http://www.w3.org/2000/svg"
+            aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c" /><h3 x="50%" y="50%" fill="#eceeef"
+              dy=".3em">{{produit.name}}</h3>
+            <img width="100%" height="100%" fill="#55595c" />
+          
+
+          <div class="card-body">
+            <p class="card-text">{{produit.description}}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary"><img src="../assets/pouceHaut.png" width="25" height="25"></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary"><img src="../assets/pouceBas.png" width="25" height="25"></button>
+              </div>
+              <small class="text-muted">{{produit.time}}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  
+  data() {
+      return {
+      produits: [],
+      error: []
+    }
+  },
+  created() {
+    axios
+      .get(`http://localhost:8080/produit.json`)
+      .then(response => {this.produits= response.data.produits})
+      .catch(e => {this.errors.push(e)})
+   }  
+}
+
+</script>
+
+<style scoped>
+  h3{
+    margin-top: 20px;
+  }
+</style>  
---
+++ livre-vue/src/components/navbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Librairie M</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+          aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto mb-2 mb-md-0">
+            <li class="nav-item active">
+              <a class="nav-link" aria-current="page" href="#"><RouterLink to="/">Accueil</RouterLink></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><RouterLink to="/card">Livres</RouterLink></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><RouterLink to="/card">Commandes</RouterLink></a>
+            </li>
+          </ul>
+
+          <form class="d-flex">
+            <input class="form-control mr-2" type="search" placeholder="Livre/catégorie" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Rechercher</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    </div>
+</template>
+
+<script>
+ export default {
+    name: "navbar"
+ }
+</script>
+
+<style scoped>
+#bodyApp{
+  padding: 0px;
+}
+.navbar .container-fluid .collapse .navbar-nav .nav-item .nav-link a{
+ text-decoration: none;
+ color: rgb(144, 144, 144);
+}
+.navbar .container-fluid .collapse .navbar-nav .nav-item .nav-link a:hover{
+  color: white;
+}
+</style>
